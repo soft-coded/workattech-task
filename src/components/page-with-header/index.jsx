d21cc3d62d5f0e44ecaa6e5e7dbcbb45e006990f
soft-coded <shrutanten.work@gmail.com>
2022-02-10 16:59:19 +0530

    Add height to page-with-header

--- src/components/page-with-header/index.jsx
+++ src/components/page-with-header/index.jsx
@@ -1,8 +1,9 @@
+import "./page-with-header.css";
 import Header from "../header";
 
 export default function PageWithHeader({ children, className }) {
 	return (
-		<div className={className}>
+		<div className={`page-with-header ${className || ""}`}>
 			<Header />
 			{children}
 		</div>
